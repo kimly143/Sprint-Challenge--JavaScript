d6f97a66659e6c1a46c0ced04825e4cdc03757ec
Kim Ly Buck <kim.3993@gmail.com>
2019-12-15 21:09:29 -0600

    finished request3

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -93,6 +93,11 @@ console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+graduates.forEach(graduate => {
+  if (graduate.university.includes("Uni")){
+    unisWithUni.push(graduate.university);
+  }
+  });
 console.log(unisWithUni);
 
 
