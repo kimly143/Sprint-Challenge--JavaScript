448b12dec3a3dd472871cca0a49c43a9c41218fd
Kim Ly Buck <kim.3993@gmail.com>
2019-12-15 22:10:23 -0600

    finished first file and all stretchs
    all methods, and functions are written under arrow funcrion, so stretch is done.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -149,7 +149,7 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
+const lowPopulationAnimals = zooAnimals.filter(animal => animal.population < 5);
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -157,13 +157,19 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce(
+  (sum,animal) => sum += animal.population,
+  0
+) 
+
+
+
 console.log(populationTotal);
 
 
 /*
 
 Stretch: If you haven't already, convert your array method callbacks into arrow functions.
-
+all methods are arrow functions
 */
 
